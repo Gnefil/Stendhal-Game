1e45f7655e216ab04bc97ac224ac60c0273c30f2
Ruben Bos <ruben.bos@student.manchester.ac.uk>
2021-12-01 10:50:33 +0000

    Add logic for BuyFurniuteChatAction

--- src/games/stendhal/server/maps/quests/houses/BuyFurnitureChatAction.java
+++ src/games/stendhal/server/maps/quests/houses/BuyFurnitureChatAction.java
@@ -5,9 +5,8 @@ import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.maps.quests.houses.HouseUtilities;
+import marauroa.common.game.SlotIsFullException;
 import games.stendhal.server.entity.item.Item;
-import games.stendhal.server.entity.mapstuff.chest.StoredChest;
 import games.stendhal.server.entity.mapstuff.portal.HousePortal;
 
 public class BuyFurnitureChatAction implements ChatAction {
@@ -15,15 +14,36 @@ public class BuyFurnitureChatAction implements ChatAction {
 	private int cost;
 	private String itemName;
 	
-	BuyFurnitureChatAction(final int cost, final String itemName) {
-		//this.cost = cost;
-		//this.itemName = itemName;
+	BuyFurnitureChatAction(final int costIn, final String itemNameIn) {
+		cost = costIn;
+		itemName = itemNameIn;
 	}
 	
 	@Override
 	public void fire(Player player, Sentence sentence, EventRaiser raiser) {
-		// TODO Auto-generated method stub
-
+		
+		HousePortal playerHouse = HouseUtilities.getPlayersHouse(player);
+		if(playerHouse == null) {
+			raiser.say("You don't own a house for me to deliver this " + itemName + " to!");
+			return;
+		}
+		final Item item = SingletonRepository.getEntityManager().getItem(itemName);
+		if(item != null) {
+			if(!player.isEquipped("money", this.cost)) {
+				raiser.say("Hang on, you can't afford this " + itemName + "!");
+				return;
+			}
+			
+			try{
+				HouseUtilities.findChest(playerHouse).add(SingletonRepository.getEntityManager().getItem(itemName));//adds item to chest found in playerhouse
+				raiser.say("I will deliver this " + itemName + " to " + playerHouse.getDoorId() + ". You should find it in your chest, though it won't be ready to use yet.");
+				player.drop("money", cost);
+			}
+			catch(SlotIsFullException e){
+				raiser.say("Sorry, I couldn't make this delivery. Make sure that you've got space in your chest and ask me again.");
+			}
+		}
+		
 	}
 
 }
