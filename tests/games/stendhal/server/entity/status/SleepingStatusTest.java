6293b1318c6acc3b3cb59c86830b66a9db9cb56f
v52937mr <mishal.rizvi@student.manchester.ac.uk>
2021-12-02 13:44:32 +0000

    Test for sleeping status

---
+++ tests/games/stendhal/server/entity/status/SleepingStatusTest.java
@@ -0,0 +1,17 @@
+package games.stendhal.server.entity.item.behavior;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import games.stendhal.server.entity.player.Player;
+import utilities.PlayerTestHelper;
+
+public class SleepingStatusTest {
+	
+	@Test
+	public void sleepStatusExists {
+		Player bob = PlayerTestHelper.createPlayer("bob");
+		SleepingStatus sleep = new SleepingStatus();
+		bob.getStatusList().inflictStatus(sleep, null);
+		assertTrue(bob.hasStatus(StatusType.SLEEPING));
+		
+	}
+}
