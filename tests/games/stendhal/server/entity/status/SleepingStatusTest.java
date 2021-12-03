980dbf2ea172f55d710ed7ec40b11aa5bce9e75d
p16862ae <anica.espanola@student.manchester.ac.uk>
2021-12-03 12:42:04 +0000

    Fix Sleeping Status Test

--- tests/games/stendhal/server/entity/status/SleepingStatusTest.java
+++ tests/games/stendhal/server/entity/status/SleepingStatusTest.java
@@ -1,4 +1,4 @@
-package games.stendhal.server.entity.item.behavior;
+package games.stendhal.server.entity.status;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import games.stendhal.server.entity.player.Player;
@@ -7,9 +7,9 @@ import utilities.PlayerTestHelper;
 public class SleepingStatusTest {
 	
 	@Test
-	public void sleepStatusExists {
+	public void sleepStatusExists() {
 		Player bob = PlayerTestHelper.createPlayer("bob");
-		SleepingStatus sleep = new SleepingStatus();
+		SleepingStatus sleep = new SleepingStatus(0, 0, 0);
 		bob.getStatusList().inflictStatus(sleep, null);
 		assertTrue(bob.hasStatus(StatusType.SLEEPING));
 		
